1ef1a324823315065a5905eec5a7bac237b02753
0natan01 <natangamermouzinho@gmail.com>
2025-07-08 00:42:07 -0300

    primeiro commit

---
+++ src/main/java/com/example/projeto_agendador_de_tarefas/ProjetoAgendadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_agendador_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAgendadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAgendadorDeTarefasApplication.class, args);
+	}
+
+}
